0147069b992bb229abcd1321ea7c70e6a95415a3
Sylvain Bertrand <sbertrand@ihmc.us>
2021-09-16 14:18:31 -0500

    :arrow_up: ihmc-realtime 1.4.0

--- ihmc-motors-core/build.gradle.kts
+++ ihmc-motors-core/build.gradle.kts
@@ -17,7 +17,7 @@ ihmc {
 }
 
 mainDependencies {
-   api("us.ihmc:ihmc-realtime:1.3.1")
+   api("us.ihmc:ihmc-realtime:1.4.0")
    api("us.ihmc:ihmc-ethercat-master:0.12.0")
    api("us.ihmc:ihmc-robot-data-logger:0.20.9")
    api("us.ihmc:ihmc-robot-data-visualizer:source")
--- ihmc-tmotor-core/build.gradle.kts
+++ ihmc-tmotor-core/build.gradle.kts
@@ -19,7 +19,7 @@ ihmc {
 mainDependencies {
    api("us.ihmc:ihmc-motors-core:source")
    api("us.ihmc:ihmc-ethercat-master:0.12.0")
-   api("us.ihmc:ihmc-realtime:1.3.1")
+   api("us.ihmc:ihmc-realtime:1.4.0")
    api("org.scream3r:jssc:2.8.0")
 }
 
