92d03691c295b530f5ca6e00c87310f02c5d8b69
Dexton Anderson <danderson@ihmc.org>
2024-02-28 12:13:07 -0600

    Add back entrypoints

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.ihmc-ci") version "8.3"
+   id("us.ihmc.ihmc-cd") version "1.26"
 }
 
 ihmc {
@@ -28,3 +29,10 @@ testDependencies {
    api(ihmc.sourceSetProject("main"))
    api("us.ihmc:ihmc-commons-testing:0.32.0")
 }
+
+tasks.getByPath("installDist").dependsOn("compositeJar")
+
+app.entrypoint("GyemsMotorTestBed", "us.ihmc.gyemsCore.teststands.GyemsMotorTestBed")
+app.entrypoint("FootsoleTestBed", "us.ihmc.teststands.FootsoleTestBed")
+app.entrypoint("TMotorKtTestBed", "us.ihmc.teststands.TMotorKtTestBed")
+app.entrypoint("TMotorTestBed", "us.ihmc.teststands.TMotorTestBed")
