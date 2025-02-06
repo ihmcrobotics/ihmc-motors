05aa6086777a4163eb4ef703235745f6a9a05360
Dexton Anderson <danderson@ihmc.org>
2025-02-06 13:45:48 -0600

    :arrow_up: ihmc-yovariables 0.13.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val ihmcOpenRoboticsSoftwareVersion = "0.14.0-240126"
 mainDependencies {
    api("us.ihmc:ihmc-ethercat-master:0.16.0")
    api("us.ihmc:ihmc-realtime:1.6.0")
-   api("us.ihmc:ihmc-yovariables-filters:0.13.5")
+   api("us.ihmc:ihmc-yovariables-filters:0.13.6")
    api("us.ihmc:ihmc-javafx-toolkit:17-0.22.12")
    api("org.scream3r:jssc:2.8.0")
    api("us.ihmc:ihmc-robot-data-visualizer:$ihmcOpenRoboticsSoftwareVersion")
